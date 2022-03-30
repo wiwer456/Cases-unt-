a8373099f89e85fd9663fcfafec3dca46df2d4d3
wiwer456 <67992786+wiwer456@users.noreply.github.com>
2022-03-30 10:40:55 +0900

    Add files via upload

---
+++ Checker3.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checker3 : MonoBehaviour
+{
+    int razr = 0;
+    Transform case_transform;
+    // Start is called before the first frame update
+    void Start()
+    {
+       case_transform = GetComponent<Transform>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(razr == 1){
+            case_transform.Translate(0, -0.1f, 0);
+        }
+    }
+    void OnMouseDown(){
+            print("мимо");
+            razr += 1;
+        
+    }
+}
